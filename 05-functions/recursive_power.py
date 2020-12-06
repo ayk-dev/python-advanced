a8262314bc07952c2f20f080089d7f25289f8009
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2020-12-06 13:21:52 -0600

    Add files via upload

---
+++ 05-functions/recursive_power.py
@@ -0,0 +1,12 @@
+def recursive_power(number, power):
+    if power == 1:
+        result = number
+    else:
+        result = number * recursive_power(number, power - 1)
+    return result
+
+print(recursive_power(2, 10))
+print(recursive_power(10, 100))
+
+
+

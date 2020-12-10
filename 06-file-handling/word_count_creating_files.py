6f461ba16722ce8a96fe522eef65925db70f7379
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2020-12-10 15:29:44 -0600

    Add files via upload

---
+++ 06-file-handling/word_count_creating_files.py
@@ -0,0 +1,9 @@
+with open('words.txt', 'w') as f:
+    f.write('quick ')
+    f.write('is ')
+    f.write('fault')
+
+with open('input.txt', 'w') as input_f:
+    input_f.write("-I was quick to judge him, but it wasn't his fault.\n")
+    input_f.write("-Is this some kind of joke?! Is it?\n")
+    input_f.write("-Quick, hide here...It is safer.\n")
